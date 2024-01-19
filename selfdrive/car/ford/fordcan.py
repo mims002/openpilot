d6da95a6d3107ac3452ce7b15b497b5159e72192
Mrinmoy <Mrinmoy002@gmail.com>
2024-01-18 23:40:17 -0500

    test

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -59,7 +59,7 @@ def create_lka_msg(packer, CAN: CanBus, lat_active: bool, apply_angle: float, cu
     'LdwActvIntns_D_Req': 2,
   }
 
-  return packer.make_can_msg("Lane_Assist_Data1", CAN.main, values)
+  return packer.make_can_msg("Lane_Assist_Data1", CAN.main, {})
 
 
 def create_lat_ctl_msg(packer, CAN: CanBus, lat_active: bool, lateral_motion_control):
@@ -90,7 +90,7 @@ def create_lat_ctl_msg(packer, CAN: CanBus, lat_active: bool, lateral_motion_con
   values = {
     "LatCtlRng_L_Max": lateral_motion_control["LatCtlRng_L_Max"],                       # Unknown [0|126] meter
     "HandsOffCnfm_B_Rq": lateral_motion_control["HandsOffCnfm_B_Rq"],                     # Unknown: 0=Inactive, 1=Active [0|1]
-    "LatCtl_D_Rq": 1 if lat_active else 0,      # Mode: 0=None, 1=ContinuousPathFollowing, 2=InterventionLeft,
+    "LatCtl_D_Rq": 3 if lateral_motion_control["LatCtlCurv_NoRate_Actl"] > 0 else 3,      # Mode: 0=None, 1=ContinuousPathFollowing, 2=InterventionLeft,
                                                 #       3=InterventionRight, 4-7=NotUsed [0|7]
     "LatCtlRampType_D_Rq": lateral_motion_control["LatCtlRampType_D_Rq"],                   # Ramp speed: 0=Slow, 1=Medium, 2=Fast, 3=Immediate [0|3]
                                                 #             Makes no difference with curvature control
